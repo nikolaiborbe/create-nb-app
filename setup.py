78d037206c626096ad09fc2cd2fae456cd0d145f
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2025-03-02 13:56:47 +0100

    forgor version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="create-nb-app",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "questionary",
